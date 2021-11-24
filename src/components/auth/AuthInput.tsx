2d96f39c305ad996c5e8c7bbd939d43b796ee97e
markilha <markilha@gmail.com>
2021-11-24 15:01:33 -0300

    Avatar Usuário

--- src/components/auth/AuthInput.tsx
+++ src/components/auth/AuthInput.tsx
@@ -1,28 +1,28 @@
-interface AuthInputProps{
-    label:string
-    valor:any
+interface AuthInputProps {
+    label: string
+    valor: any
     obrigatorio?: boolean
     naoRenderizarQuando?: boolean
-    valorMudou: (novovalor:any)=>void
-    tipo: 'text' | 'email'|'password'
-    
+    valorMudou: (novovalor: any) => void
+    tipo: 'text' | 'email' | 'password'
+
 }
 
-export default function AuthInput(props:AuthInputProps){
-    return props.naoRenderizarQuando ? null :(
+export default function AuthInput(props: AuthInputProps) {
+    return props.naoRenderizarQuando ? null : (
         <div className="flex flex-col mt-4">
             <label>{props.label}</label>
-                <input 
+            <input
                 type={props.tipo ?? 'text'}
                 value={props.valor}
-                onChange={e=> props.valorMudou?.(e.target.value)}
-                required ={props.obrigatorio}
+                onChange={e => props.valorMudou?.(e.target.value)}
+                required={props.obrigatorio}
                 className={`
                 px-4 py-3 rounded-lg bg-gray-200 mt-2
                 border focus:border-blue-500 focus:bg-white
                 focus:outline-none
                 `}
-                /> 
+            />
         </div>
     )
 }
--- src/pages/autenticacao.tsx
+++ src/pages/autenticacao.tsx
@@ -1,25 +1,29 @@
 import { useState } from "react";
 import AuthInput from "../components/auth/AuthInput";
 import { IconeAtencao } from "../components/icons";
+import useAuth from "../data/hook/useAuth";
 
 
 
 export default function Autenticacao() {
+
+    const { usuario, loginGoogle } = useAuth()
+
     const [erro, setErro] = useState(null)
     const [modo, setModo] = useState<'login' | 'cadastro'>('login')
     const [email, setEmail] = useState('')
     const [senha, setSenha] = useState('')
 
-    function exibirErro(msg,tempoEmSegundos=5){
+    function exibirErro(msg, tempoEmSegundos = 5) {
         setErro(msg)
-        setTimeout(() => setErro(null),tempoEmSegundos* 1000);
+        setTimeout(() => setErro(null), tempoEmSegundos * 1000);
     }
-    
+
     function submeter() {
         if (modo == 'login') {
             exibirErro('Ocorreu um erro no login')
         } else {
-            exibirErro('Ocorreu um erro no cadastro')            
+            exibirErro('Ocorreu um erro no cadastro')
         }
     }
 
@@ -27,10 +31,10 @@ export default function Autenticacao() {
 
         <div className="flex h-screen items-center justify-center">
             <div className=" hidden md:block md:w-1/2 lg:w-2/3"  >
-                <img 
-                src="https://source.unsplash.com/random" 
-                alt="Imagem da Tela de Autenticação"
-                className="h-screen w-full object-cover"
+                <img
+                    src="https://source.unsplash.com/random"
+                    alt="Imagem da Tela de Autenticação"
+                    className="h-screen w-full object-cover"
                 />
             </div>
             <div className=" m-10 w-full md:w-1/2 lg:w-1/3">
@@ -41,18 +45,18 @@ export default function Autenticacao() {
                 </h1>
 
                 {erro ? (
-                     <div className={`
+                    <div className={`
                      flex items-center 
                      bg-red-400 text-white py-3 px-5 my-2
                      border border-red-700 rounded-lg
                      `}>
-                         {IconeAtencao(6)}   
-                         <span className="ml-3">{erro}</span>                 
-     
-                     </div>
+                        {IconeAtencao(6)}
+                        <span className="ml-3">{erro}</span>
 
-                ):false}
-               
+                    </div>
+
+                ) : false}
+
 
                 <AuthInput
                     label="Email"
@@ -76,30 +80,30 @@ export default function Autenticacao() {
                 </button>
                 <hr className="my-6 border-gray-300 w-full" />
 
-                <button onClick={submeter} className={`
+                <button onClick={loginGoogle} className={`
             w-full bg-red-500 hover:bg-red-400
             text-white rounded-lg px-4 py-3 
             `}>
                     Entrar no Google
                 </button>
 
-                {modo=='login'? (
+                {modo == 'login' ? (
                     <p className="mt-8">
                         Novo por aqui?
-                        <a onClick={()=> setModo('cadastro')} className={`
+                        <a onClick={() => setModo('cadastro')} className={`
                         text-blue-500 hover:text-blue-700 font-semibold
                         cursor-pointer
                         `}> Criar uma conta gratuitamente </a>
-                    </p>                   
+                    </p>
 
-                ):(
+                ) : (
                     <p className="mt-8">
-                       Já faza parte da nossa comunidade?
-                        <a onClick={()=> setModo('login')} className={`
+                        Já faza parte da nossa comunidade?
+                        <a onClick={() => setModo('login')} className={`
                         text-blue-500 hover:text-blue-700 font-semibold
                         cursor-pointer
                         `}> Entre com a suas credenciais</a>
-                    </p> 
+                    </p>
                 )}
 
             </div>
